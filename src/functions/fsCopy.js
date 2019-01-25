195b9ef7df86a33b0e7b375736d66fa457f84f95
mccarthydanielle <daniellemccarthy54@gmail.com>
2019-01-25 18:02:52 -0500

    updated copy function

--- src/functions/fsCopy.js
+++ src/functions/fsCopy.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra')
 
 const path = require('path')
 
-const copy = () => fs.copy(path.join(__dirname, '../renderer/mobiletemp'), '/Users/milleniumfalcon/Documents/FullStack/CapStone/electron-webpack-quick-start/src/renderer/mynewdir', function (err) {
+const copy = () => fs.copy(path.join(__dirname, '../../template/mobiletemp'), './copyOfProject/', function (err) {
   if (err) {
     console.error(err)
   } else {
