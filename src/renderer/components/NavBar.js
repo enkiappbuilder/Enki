2984f56f458f72e48fe61e18fe1275fad1592e5e
Barry Huang <Barry.huang93@gmail.com>
2019-01-28 12:32:14 -0500

    sidebar with logo

--- src/renderer/components/NavBar.js
+++ src/renderer/components/NavBar.js
@@ -1,19 +1,20 @@
 import React from "react";
 import path from "path";
 import { Sidebar, Menu, Segment, Header, Icon , Image} from "semantic-ui-react";
-const logopath = 'file://Users/milleniumfalcon/Documents/FullStack/CapStone/Enki/static/logo.png'
+import logopath from '/Users/milleniumfalcon/Documents/FullStack/CapStone/Enki/static/logo.png'
 // const logo = require(logopath)
 const SideBar = () => {
   return(
   <Sidebar.Pushable as={Segment}>
-    <Image src={logopath}/> 
-    <Sidebar as={Menu} animation="slide along" vertical width={"very thin"}>
+    <Sidebar as={Menu} animation="slide along" vertical visible width='thin'>
+    <Image src={logopath} /> 
       <Menu.Item> HI I AM ITEM </Menu.Item>
     </Sidebar>
 
     <Sidebar.Pusher>
       <Segment basic>
         <Header as="h3">Application Content</Header>
+
       </Segment>
     </Sidebar.Pusher>
   </Sidebar.Pushable>)
