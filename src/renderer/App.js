81c3e5d347f27bca208b78f90d0faa9efc53ef38
Barry Huang <Barry.huang93@gmail.com>
2019-01-28 17:19:27 -0500

    closes #25

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Routes from './routes'
+import { EditPage } from "./components";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Routes />
+        {/* <Routes /> */}
+        <EditPage/>
       </div>
     );
   }
