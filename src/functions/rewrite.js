75cc25e2cf6ddb086f5a473f24801ee53f0ae63f
mccarthydanielle <daniellemccarthy54@gmail.com>
2019-01-29 11:16:48 -0500

    added working fn

--- src/functions/rewrite.js
+++ src/functions/rewrite.js
@@ -1,19 +1,22 @@
 const fs = require('fs')
 const path = require('path')
 
-//comment format 1 (comments are in this format: {/*text1*/)
+//comment format 1 (for comments in this format: {/*text1*/)
 
 //-------Example-------//
+
 // BEFORE --- <Text> {/*textBox1*/} Hello, my name is Bob! {/*textBox1*/}</Text>
 // AFTER --- <Text> {/*textBox1*/} Bye, see you later! {/*textBox1*/} </Text>
 
-function updateText(fileName, location, replacementText) {
+//example of calling this function below
+//updateText('../../copyOfProject/screens/Gallery.js', 'text2', 'wooo!!')
+
+export function updateText(fileName, location, replacementText) {
   let tempRegex = "(?<={/\\*" + location + "\\*/})(.*)(?={/\\*" + location + "\\*/})"
   let regex = new RegExp(tempRegex)
-
   fs.readFile(path.join(__dirname, fileName), 'utf8', function (err, data) {
     if (err) throw err
-    content = data
+    let content = data
     content = content.replace(regex, replacementText)
     fs.writeFile(path.join(__dirname, fileName), content, 'utf8', function (err) {
       if (err) throw err
@@ -22,25 +25,4 @@ function updateText(fileName, location, replacementText) {
   })
 }
 
-// comment format 2 (comments are in this format: /*image1*/) AND you would like a string to be the replacement text.
-
-//-------Example-------//
-// BEFORE --- const image = require( /*image1*/ "../this/is/my/path" /*image1*/)
-// AFTER --- const image = require( /*image1*/ "../new/path" /*image1*/)
-
-
-function updateFormat2(fileName, location, replacementText) {
-  let tempRegex = "(?<= /\\*" + location + "\\*/)(.*)(?=/\\*" + location + "\\*/)"
-  let regex = new RegExp(tempRegex)
-
-  fs.readFile(path.join(__dirname, fileName), 'utf8', function (err, data) {
-    if (err) throw err
-    content = data
-    content = content.replace(regex, ` "${replacementText}" `)
-    fs.writeFile(path.join(__dirname, fileName), content, 'utf8', function (err) {
-      if (err) throw err
-      console.log('FILE SAVED')
-    })
-  })
-}
 
