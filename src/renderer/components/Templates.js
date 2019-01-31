9f6cbf880728e08dfc0d3e63c0a2a0b98287dc87
zhhjoseph <zhhjoseph@gmail.com>
2019-01-31 15:32:01 -0500

    basic template layout done

--- src/renderer/components/Templates.js
+++ src/renderer/components/Templates.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom'
-import { Header, Container, Button, Grid } from 'semantic-ui-react'
+import { Header, Container, Button, Grid, Item } from 'semantic-ui-react'
 import TemplateContainer from './TemplateContainer'
+import ice from './image/ice.jpg'
 import {showSideBar} from '../store/sideBar'
 import {connect} from 'react-redux'
 
@@ -15,33 +16,70 @@ class Templates extends Component {
   }
   render() {
     return (
+      <div style={{ backgroundColor: '#D1FFD7', maxHeight: '100vh', maxWidth: '100vw', overflow: "scroll" }}>
+
       <Container style={styles.containerStyle}>
-        <Header style={styles.headerStyle}>Create your app</Header>
         <Header style={styles.subHeaderStyle}>Choose a template</Header>
 
         {/* these containers are buttons to the customize project page, we will need to add functionality to copy the appropriate template when they are pressed*/}
-        <Grid columns='equal'>
+        <Grid columns='1'>
           <Grid.Column>
-            <Link to='/create'>
-              <TemplateContainer header='Artist App' />
-            </Link>
+            <Item.Group>
+              <Item>
+                <Item.Image size='medium' src = {ice}/>
+                <Item.Content>
+                  <Item.Header>
+                    Artists
+                  </Item.Header>
+                  <Item.Description>
+                    This is some description for the user
+                  </Item.Description>
+                </Item.Content>
+              </Item>
+              <text style = {styles.mobileTitle}>Artists</text>
+                <Button>Link to create template</Button>
+                <Button>Button to preview modal</Button>
+            </Item.Group>
+            <Item.Group>
+              <Item>
+                <Item.Image size='medium' src = {ice}/>
+                <Item.Content>
+                  <Item.Header>
+                    Artists
+                  </Item.Header>
+                  <Item.Description>
+                    This is some description for the user
+                  </Item.Description>
+                </Item.Content>
+              </Item>
+              <text style = {styles.mobileTitle}>Artists</text>
+                <Button>Link to create template</Button>
+                <Button>Button to preview modal</Button>
+            </Item.Group>
+            <Item.Group>
+              <Item>
+                <Item.Image size='medium' src = {ice}/>
+                <Item.Content>
+                  <Item.Header>
+                    Artists
+                  </Item.Header>
+                  <Item.Description>
+                    This is some description for the user
+                  </Item.Description>
+                </Item.Content>
+              </Item>
+              <text style = {styles.mobileTitle}>Artists</text>
+                <Button>Link to create template</Button>
+                <Button>Button to preview modal</Button>
+            </Item.Group>
           </Grid.Column>
 
-
-          <Grid.Column>
             <Link to='/create'>
-              <TemplateContainer header='Restaurant App' />
             </Link>
-          </Grid.Column>
-
-          <Grid.Column>
-            <Link to='/create'>
-              <TemplateContainer header='Small Business App' />
-            </Link>
-          </Grid.Column>
-
+              {/* <TemplateContainer /> */}
         </Grid>
       </Container>
+      </div>
 
     );
   }
@@ -60,7 +98,6 @@ const styles = {
   containerStyle: {
     alignContent: 'center',
     margin: 20,
-    textAlign: 'center'
   },
 
   headerStyle: {
@@ -70,5 +107,8 @@ const styles = {
   subHeaderStyle: {
     fontSize: 25,
     color: '#4a4b4c'
+  },
+  mobileTitle: {
+    fontSize: 40
   }
 }
