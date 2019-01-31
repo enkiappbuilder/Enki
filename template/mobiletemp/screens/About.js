8a6499a740474a41ba06d60c880cc3dfe5bbd648
mccarthydanielle <daniellemccarthy54@gmail.com>
2019-01-31 14:21:47 -0500

    aboutMeStyled

--- template/mobiletemp/screens/About.js
+++ template/mobiletemp/screens/About.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {
-  ScrollView,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View,
+  Image
+
 } from 'react-native';
 import {
-  Header
+  Header,
+  Button,
 } from 'react-native-elements'
-import { Icon } from 'react-native-elements'
+
+import Hamburger from '../components/Hamburger'
 export default class About extends React.Component {
   static navigationOptions = {
     header: 'About me!',
@@ -20,92 +22,72 @@ export default class About extends React.Component {
       <View style={styles.container}>
         <Header
           leftComponent={<Hamburger navigationProps={this.props.navigation} />}
-          centerComponent={{ text: 'Joe Hu', style: { color: '#132029' } }}
+          centerComponent={{ text: /*AboutMeHeaderText*/'Joe Hu'/*AboutMeHeaderText*/, style: { color: '#132029' } }}
           rightComponent={{ icon: 'home', color: '#132029', onPress: () => this.props.navigation.navigate('Home') }}
           backgroundColor='rgba(250,249,249,0.8)'
         />
-
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.textComp}>
-            <Text style={{ color: 'white', textAlign: 'center', fontSize: 35 }}>
-              About Me
+        <View style={styles.flex}>
+          <Image
+            style={styles.aboutMeImg}
+            source={{ uri: /*AboutMeImg*/ "https://cloud.fullstackacademy.com/Corey.jpg?mtime=20170626111607" /*AboutMeImg*/ }}
+            resizeMode="contain"
+            borderRadius={20}
+          />
+          <View style={styles.textBorder}>
+            <Text style={styles.aboutMeHeader}>
+              {/*AboutMeHeader*/} About Me {/*AboutMeHeader*/}
+            </Text>
+            <Text style={styles.text}>
+              {/*AboutMeDescText*/}This is just some filler lorem ipsum, no not really, but this is an about page that should be something about you! Now make up your mind and say something about yourself! Probably something good and not bad! Yeah! {/*AboutMeDescText*/}
             </Text>
           </View>
-          <View style={styles.textComp}>
-            <Text style={{ color: 'white', }}>
-              This is just some filler lorem ipsum, no not really, but this is an about page that should be something about you! Now make up your mind and say something about yourself! Probably something good and not bad! Yeah!
-            </Text>
-          </View>
-        </ScrollView>
+          <Button
+            onPress={() => this.props.navigation.navigate('Contact')}
+            title= /*AboutMeButtonText*/ 'Contact' /*AboutMeButtonText*/
+            outline
+            style={styles.homePageButton}
+          />
+        </View>
       </View>
     );
   }
 }
 
-class Hamburger extends React.Component {
-  toggleDrawer = () => {
-    this.props.navigationProps.toggleDrawer();
-  }
-
-  render() {
-    return (
-      <View style={{ flexDirection: 'row' }}>
-        <TouchableOpacity onPress={this.toggleDrawer.bind(this)}>
-          <Icon
-            name='menu'
-          />
-        </TouchableOpacity>
-      </View>
-    )
-  }
-}
-
 const styles = StyleSheet.create({
+  homePageButton: {
+    backgroundColor: /*AboutMeButtonBackgroundColor*/ 'rgba(250,249,249,0.1)' /*AboutMeButtonBackgroundColor*/,
+    marginBottom: 20
+  },
   container: {
     flex: 1,
-    backgroundColor: '#000000',
+    backgroundColor: /*AboutMeBackgroundColor*/ '#0e0f0f' /*AboutMeBackgroundColor*/,
     color: '#fff'
   },
-  textComp: {
-    backgroundColor: '#000000',
-    alignContent: 'center'
-
+  textBorder: {
+    borderColor: /*AboutMeTextBorderColor*/ 'blue' /*AboutMeTextBorderColor*/,
+    borderWidth: 1,
+    margin: 10,
+    backgroundColor: /*AboutMeTextBackgroundColor*/ '#0e0f0f' /*AboutMeTextBackgroundColor*/
   },
-  cardContainer: {
+  flex: {
     flex: 1,
-    backgroundColor: '#000000'
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
+  aboutMeImg: {
+    flex: 1,
+    alignSelf: 'stretch',
+    margin: 20
+  },
+  aboutMeHeader: {
+    color: /*AboutMeHeaderTextColor*/ 'white' /*AboutMeHeaderTextColor*/,
     textAlign: 'center',
+    fontSize: 35,
+    margin: 5
   },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    width: '100%',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  navigationFilename: {
-    marginTop: 5,
+  text: {
+    margin: 10,
+    color: /*AboutMeDescTextColor*/ 'white' /*AboutMeDescTextColor*/,
+    textAlign: 'center'
   }
 });
