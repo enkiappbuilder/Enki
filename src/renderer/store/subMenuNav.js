72079d8d6b5c4a09f3640ad557c334cc82a12ec6
zhhjoseph <zhhjoseph@gmail.com>
2019-02-02 15:28:34 -0500

    created action types for submenu nav

---
+++ src/renderer/store/subMenuNav.js
@@ -0,0 +1,32 @@
+//action types
+const SHOW_HOME = "SHOW_HOME"
+const SHOW_GALLERY = "SHOW_GALLERY"
+const SHOW_ABOUT = "SHOW_GALLERY"
+const SHOW_CONTACT = "SHOW_CONTACT"
+const SHOW_HELP = "SHOW_HELP"
+
+//action creators
+export const showMENU = () => {
+  return {
+    type: SHOW_MENU_BAR,
+    menuVisible: true
+  };
+};
+
+export const hideMenu = () => {
+  return {
+    type: HIDE_MENU_BAR,
+    menuVisible: false
+  };
+};
+
+export default function(state = false, action) {
+  switch (action.type) {
+    case SHOW_MENU_BAR:
+      return action.menuVisible;
+    case HIDE_MENU_BAR:
+      return action.menuVisible;
+    default:
+      return state;
+  }
+}
