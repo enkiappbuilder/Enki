71a9ab77968c2d94263e6bad792cd1e17250870c
mccarthydanielle <daniellemccarthy54@gmail.com>
2019-02-04 16:40:26 -0500

    color picker shows currently selected color

--- src/renderer/components/Forms.js
+++ src/renderer/components/Forms.js
@@ -1,26 +1,25 @@
 import React from 'react'
 import { Form, Button } from 'semantic-ui-react'
 import { updateText } from '../../functions/rewrite'
+import ColorPicker from './ColorPicker'
 
 const Forms = (props) => {
-  const {  handleChange, upState, name } = props;
-// console.log('upState:', upState);
-  let title = name.replace(/([A-Z])/g, ' $1').replace(/^./, function(str){ return str.toUpperCase(); })
-
-
+  const { handleChange, upState, name } = props;
+  let title = name.replace(/([A-Z])/g, ' $1').replace(/^./, function (str) { return str.toUpperCase(); })
 
   return (
-    <Form>
-      <Form.Field>
-        <label>{title} </label>
-        <Form.Input
-          placeholder='change this text'
-          name={name}
-          value={upState[name]}
-          onChange={handleChange} />
-      </Form.Field>
-
-    </Form>
+    <div>
+      <Form>
+        <Form.Field>
+          <label>{title}</label>
+          <Form.Input
+            placeholder='change this text'
+            name={name}
+            value={upState[name]}
+            onChange={handleChange} />
+        </Form.Field>
+      </Form>
+    </div>
   )
 }
 
