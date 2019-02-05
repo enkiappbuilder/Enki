ebb69c9f94bbecfceb239862e8b5e841fa015d8e
mccarthydanielle <daniellemccarthy54@gmail.com>
2019-02-04 22:32:54 -0500

    added redux logic to conditional rendering of edit page and removed console.logs throughout

--- src/renderer/components/PreviewModal.js
+++ src/renderer/components/PreviewModal.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button, Header, Modal, Image } from 'semantic-ui-react'
 
 class ModalPreview extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       open: false
@@ -11,46 +11,45 @@ class ModalPreview extends React.Component {
     this.close = this.close.bind(this)
   }
 
-  closeModal (closeOnEscape){
-    this.setState({closeOnEscape, open: true})
+  closeModal(closeOnEscape) {
+    this.setState({ closeOnEscape, open: true })
   }
 
   close() {
-    this.setState({open: false})
+    this.setState({ open: false })
   }
-  render(){
-    console.log('MODAL STATE', this.state)
-    const {open, closeOnEscape} = this.state
+  render() {
+    const { open, closeOnEscape } = this.state
     return (
 
-    <Modal
-    open = {open}
-    style={{ width: "50%" }}
-    trigger={<Button onClick={()=> this.closeModal(false)}>Preview App</Button>} size="fullscreen"
-    basic
-    closeOnEscape = {closeOnEscape}
-    onClose = {this.close}>
-      <Header textAlign='center' color='green' content='App Preview' />
-      <Modal.Content image style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-        <div style={{ display: 'flex', justifyContent: 'space-around' }} >
-          {this.props.images.map(image => {
-            return (
-              <div>
-                <Image src={image} />
-              </div>
-            )
-          })}
-        </div>
-        <Modal.Actions>
+      <Modal
+        open={open}
+        style={{ width: "50%" }}
+        trigger={<Button onClick={() => this.closeModal(false)}>Preview App</Button>} size="fullscreen"
+        basic
+        closeOnEscape={closeOnEscape}
+        onClose={this.close}>
+        <Header textAlign='center' color='green' content='App Preview' />
+        <Modal.Content image style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+          <div style={{ display: 'flex', justifyContent: 'space-around' }} >
+            {this.props.images.map(image => {
+              return (
+                <div>
+                  <Image src={image} />
+                </div>
+              )
+            })}
+          </div>
+          <Modal.Actions>
 
-          <Button
-          color = 'red'
-          onClick={this.close}
-          content = 'Back to Templates'
-          ></Button>
-        </Modal.Actions>
-      </Modal.Content>
-    </Modal>
+            <Button
+              color='red'
+              onClick={this.close}
+              content='Back to Templates'
+            ></Button>
+          </Modal.Actions>
+        </Modal.Content>
+      </Modal>
     )
   }
 }
