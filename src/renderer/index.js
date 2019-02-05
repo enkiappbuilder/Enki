7b78c1244752eadd1144403a9175a8b8884f64bf
mccarthydanielle <daniellemccarthy54@gmail.com>
2019-02-05 15:10:22 -0500

    Added ENKI to landing page

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -13,6 +13,7 @@ ReactDOM.render(
       rel="stylesheet"
       href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css"
     />
+    <link href="https://fonts.googleapis.com/css?family=Bangers|Bungee+Inline|Fredericka+the+Great|Londrina+Outline|Monoton" rel="stylesheet"></link>
 
     <Provider store={store}>
       <Router>
