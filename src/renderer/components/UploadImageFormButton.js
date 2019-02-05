a4217429930d3009904c111de4e579569d9bd44b
zhhjoseph <zhhjoseph@gmail.com>
2019-02-05 16:32:35 -0500

    few form fixes

--- src/renderer/components/UploadImageFormButton.js
+++ src/renderer/components/UploadImageFormButton.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Form, Button } from 'semantic-ui-react'
+// import { styles } from 'ansi-colors';
 
 const UploadImage = (props) => {
   const { handleChange, upState, name } = props;
   let title = name.replace(/([A-Z])/g, ' $1').replace(/^./, function (str) { return str.toUpperCase(); })
 
   return (
-    <Form>
-      <Form.Field>
+    <Form style = {styles.imageUpload}>
+      <Form.Field >
         <span>
           <label style={{ margin: 10, fontWeight: 'bold' }}>{title}   </label>
           <Button
+          color = 'blue'
             onClick={() => props.handleUpload(props.name)}
           >
             Upload image
@@ -23,3 +25,9 @@ const UploadImage = (props) => {
 }
 
 export default UploadImage
+
+const styles = {
+  imageUpload: {
+    display: 'flex',
+  }
+}
